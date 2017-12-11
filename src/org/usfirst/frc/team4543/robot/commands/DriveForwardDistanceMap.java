8d521f4f2dc6eb5181445c3f1b58f80328ff3168
Kevin Zheng <coffeevector@gmail.com>
2017-12-10 18:38:16 -0800

    Actually made DriveForwardDistacneMap command word

--- src/org/usfirst/frc/team4543/robot/commands/DriveForwardDistanceMap.java
+++ src/org/usfirst/frc/team4543/robot/commands/DriveForwardDistanceMap.java
@@ -1,23 +1,19 @@
 package org.usfirst.frc.team4543.robot.commands;
 
+import org.usfirst.frc.team4543.map.FieldMap;
+import org.usfirst.frc.team4543.map.RobotPosition;
 import org.usfirst.frc.team4543.robot.Robot;
 import org.usfirst.frc.team4543.robot.Subsystems;
 
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.CommandGroup;
 
-public class DriveForwardDistanceMap extends Command {
+public class DriveForwardDistanceMap extends CommandGroup {
 	public DriveForwardDistanceMap(double distanceOut, double power) {
-		requires(Robot.getSubSystem(Subsystems.DRIVE_TRAIN));
-		requires(Robot.getSubSystem(Subsystems.FIELD_MAP));
-	}
-
-	@Override
-	protected void execute() {
-	}
-
-	@Override
-	protected boolean isFinished() {
-		return false;
+		FieldMap fm = (FieldMap) Robot.getSubSystem(Subsystems.FIELD_MAP);
+		RobotPosition rp = fm.getRobotPosition();
+		fm.setTargetPosition(rp.getX() + distanceOut * Math.cos(rp.getTheta()),
+				rp.getY() + distanceOut * Math.cos(rp.getTheta()), false);
+		addSequential(new ToTargetPosition(power));
 	}
 
 }
