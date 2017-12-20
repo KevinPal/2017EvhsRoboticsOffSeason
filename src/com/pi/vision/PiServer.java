7e7370f6e6c9d0d98de185aebeddde4885b85dea
Kevin Palani <kevinpalani@gmail.com>
2017-12-20 00:07:34 -0800

    better libraries

--- src/com/pi/vision/PiServer.java
+++ src/com/pi/vision/PiServer.java
@@ -1,19 +1,15 @@
 package com.pi.vision;
 
-import java.awt.Frame;
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-
-import com.googlecode.javacv.FrameGrabber;
-import com.googlecode.javacv.VideoInputFrameGrabber;
-import com.googlecode.javacv.cpp.opencv_core.IplImage;
+import org.opencv.calib3d.StereoBM;
+import org.opencv.calib3d.StereoSGBM;
 
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 public class PiServer extends Thread{
+	
+
 
 	public static void main(String[] args) {
 		
@@ -22,22 +18,26 @@ public class PiServer extends Thread{
 	}
 	
 	
-	
-	
-	
-	
 	@Override
 	public void run() {
 		NetworkTable.setClientMode();
 		NetworkTable.setIPAddress("uh somehow get ip of robot"); //TODO GET robot ip
 		NetworkTable table = NetworkTable.getTable("visionTable");
-	
+		NetworkTable status = NetworkTable.getTable("robotStatus");
 		table.putString("testKey", "testValue");
+		
+		while(!status.getBoolean("isRobotInit") || !status.getBoolean("visionInit")) { //wait for robot init
+			Thread.sleep(1000);
+		}
+		
 		
 		UsbCamera cam1 = CameraServer.getInstance().startAutomaticCapture();
 		UsbCamera cam2 = CameraServer.getInstance().startAutomaticCapture();
 		
+		
+		
 		while(true) { 
+			StereoSGBM s = StereoSGBM.create(minDisparity, numDisparities, blockSize, P1, P2, disp12MaxDiff, preFilterCap, uniquenessRatio, speckleWindowSize, speckleRange, mode) //StereoSGBM apprenety gives better quality @ cost of speed (vs StereoBM)
 			
 		}
 		
