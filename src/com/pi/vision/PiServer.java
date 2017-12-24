52d06f70a85d8893e1f3075224f352a6be6d38b6
Kevin Palani <kevinpalani@gmail.com>
2017-12-23 20:38:27 -0800

    added matrix serialization

--- src/com/pi/vision/PiServer.java
+++ src/com/pi/vision/PiServer.java
@@ -1,7 +1,11 @@
 package com.pi.vision;
 
-import org.opencv.calib3d.StereoBM;
-import org.opencv.calib3d.StereoSGBM;
+import java.io.IOException;
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
 
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.CameraServer;
@@ -11,9 +15,30 @@ public class PiServer extends Thread{
 	
 
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+		for(String s:System.getProperty("java.library.path").split(";")) {
+			System.out.println(s);
+		}
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		Mat m = new Mat(2, 2, CvType.CV_64FC3);
 		
-		new PiServer().run();
+		for(int i = 0; i < m.height(); i++) {
+			for(int j = 0; j < m.width(); j++) {
+				m.put(i, j, Math.random(), Math.random(), Math.random());
+			}
+		}
+		RobotData data = new RobotData(m);
+		System.out.println(data);
+		data.updateImgData();
+		long startTime = System.nanoTime();
+		String s = Serialzier.toString(data);
+		System.out.println("Time taken: " + (System.nanoTime() - startTime)/1E9d);
+		System.out.println(s);
+		Object o = Serialzier.fromString(s);
+		RobotData serializedData = (RobotData) o;
+		serializedData.updateImage();
+		System.out.println(serializedData);
+		//new PiServer().run();
 		
 	}
 	
@@ -21,23 +46,23 @@ public class PiServer extends Thread{
 	@Override
 	public void run() {
 		NetworkTable.setClientMode();
-		NetworkTable.setIPAddress("uh somehow get ip of robot"); //TODO GET robot ip
+		NetworkTable.setIPAddress("10.45.43.2"); //TODO GET robot ip
 		NetworkTable table = NetworkTable.getTable("visionTable");
 		NetworkTable status = NetworkTable.getTable("robotStatus");
 		table.putString("testKey", "testValue");
 		
-		while(!status.getBoolean("isRobotInit") || !status.getBoolean("visionInit")) { //wait for robot init
-			Thread.sleep(1000);
-		}
+		//while(!status.getBoolean("isRobotInit") || !status.getBoolean("visionInit")) { //wait for robot init
+		//	Thread.sleep(1000);
+		//}
 		
 		
 		UsbCamera cam1 = CameraServer.getInstance().startAutomaticCapture();
-		UsbCamera cam2 = CameraServer.getInstance().startAutomaticCapture();
+		//UsbCamera cam2 = CameraServer.getInstance().startAutomaticCapture();
 		
 		
 		
 		while(true) { 
-			StereoSGBM s = StereoSGBM.create(minDisparity, numDisparities, blockSize, P1, P2, disp12MaxDiff, preFilterCap, uniquenessRatio, speckleWindowSize, speckleRange, mode) //StereoSGBM apprenety gives better quality @ cost of speed (vs StereoBM)
+			//StereoSGBM s = StereoSGBM.create(minDisparity, numDisparities, blockSize, P1, P2, disp12MaxDiff, preFilterCap, uniquenessRatio, speckleWindowSize, speckleRange, mode) //StereoSGBM apprenety gives better quality @ cost of speed (vs StereoBM)
 			
 		}
 		
