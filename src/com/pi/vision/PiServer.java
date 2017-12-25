a6dbdb65559ee41c8a2442862aa2738f4c9a6cbf
Kevin Palani <kevinpalani@gmail.com>
2017-12-25 11:12:59 -0800

    using gradle to build on pi, added llibraries

--- src/com/pi/vision/PiServer.java
+++ src/com/pi/vision/PiServer.java
@@ -14,32 +14,34 @@ import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 public class PiServer extends Thread{
 	
-
+	private final String ip = "10.45.43.101";
+	private final int port = 27;//randomly using port 27 might be bad idea
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
+		System.out.println("Searching for native, if there is an unlinked expection add the native to one of these paths");
 		for(String s:System.getProperty("java.library.path").split(";")) {
 			System.out.println(s);
 		}
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-		Mat m = new Mat(2, 2, CvType.CV_64FC3);
-		
-		for(int i = 0; i < m.height(); i++) {
-			for(int j = 0; j < m.width(); j++) {
-				m.put(i, j, Math.random(), Math.random(), Math.random());
-			}
-		}
-		RobotData data = new RobotData(m);
-		System.out.println(data);
-		data.updateImgData();
-		long startTime = System.nanoTime();
-		String s = Serialzier.toString(data);
-		System.out.println("Time taken: " + (System.nanoTime() - startTime)/1E9d);
-		System.out.println(s);
-		Object o = Serialzier.fromString(s);
-		RobotData serializedData = (RobotData) o;
-		serializedData.updateImage();
-		System.out.println(serializedData);
-		//new PiServer().run();
+//		Mat m = new Mat(2, 2, CvType.CV_64FC3);
+//		
+//		for(int i = 0; i < m.height(); i++) {
+//			for(int j = 0; j < m.width(); j++) {
+//				m.put(i, j, Math.random(), Math.random(), Math.random());
+//			}
+//		}
+//		RobotData data = new RobotData(m);
+//		System.out.println(data);
+//		data.updateImgData();
+//		long startTime = System.nanoTime();
+//		String s = Serialzier.toString(data);
+//		System.out.println("Time taken: " + (System.nanoTime() - startTime)/1E9d);
+//		System.out.println(s);
+//		Object o = Serialzier.fromString(s);
+//		RobotData serializedData = (RobotData) o;
+//		serializedData.updateImage();
+//		System.out.println(serializedData);
+		new PiServer().run();
 		
 	}
 	
@@ -50,7 +52,8 @@ public class PiServer extends Thread{
 		PiConnection connection = null;
 		
 		try {
-			connection = new PiConnection("10.45.43.101", 27); //randomly using port 27 might be bad idea
+			System.out.println("Looking for connection on " + ip + ", port " + port);
+			connection = new PiConnection(ip, port); 
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		} 
